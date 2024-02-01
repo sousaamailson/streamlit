d7a53838bdac5c13924b1eb4440539d65feeeb71
Amailson Sousa <62559446+sousaamailson@users.noreply.github.com>
2024-02-01 20:29:03 -0300

    Add files via upload

---
+++ app.py
@@ -0,0 +1,24 @@
+import streamlit as st
+import plotly.express as px
+import pandas as pd
+
+# Criando dados de exemplo
+dados = pd.DataFrame({
+    'X': [1, 2, 3, 4, 5],
+    'Y': [10, 11, 12, 13, 14]
+})
+
+# Título do aplicativo
+st.title('Exemplo de Gráfico com Streamlit')
+
+# Criando um gráfico de dispersão com Plotly Express
+fig = px.scatter(dados, x='X', y='Y', title='Gráfico de Dispersão')
+
+# Exibindo o gráfico no Streamlit
+st.plotly_chart(fig)
+
+
+
+
+
+#"C:\Users\amailson.sousa\OneDrive - ARG\Área de Trabalho\app-streamlit.py"
